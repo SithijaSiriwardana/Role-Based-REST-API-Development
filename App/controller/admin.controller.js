e81ee3680e69b9618309cb5f115f033e7c99008a
sajithperera <sithijalawan@gmail.com>
2021-11-26 11:17:13 +0530

    Instructor create API added

---
+++ App/controller/admin.controller.js
@@ -0,0 +1,41 @@
+const db = require('../config/db.config.js');
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+const env = require('../config/env.js');
+const constValues = require('../utils/constants.js');
+const User = db.users;
+
+var generator = require('generate-password');
+
+
+exports.instrcutorCreate = (req, res) => {
+	//Check Username
+    User.findOne({
+		where: {
+		  username: req.body.username
+		}
+	  }).then(user => {
+		if (user) {
+		  res.status(400).send({message: "Failed! Username is already in use!"});
+		}else{
+			//Random password generator
+			var password = generator.generate({
+				length: 10,
+				numbers: true
+			});
+
+            //create User
+			User.create({
+				username: req.body.username,
+				role: constValues.userRoles.Instrctor,
+				password: bcrypt.hashSync(password, 8)
+			})
+			.then(user => {
+				res.status(200).send({ instrutorPassword: password, message: "Instructor was created successfully!" });
+			})
+			.catch(err => {
+				res.status(500).send({ message: err.message });
+			});
+		}
+	});	
+};
--- App/middleware/auth.js
+++ App/middleware/auth.js
@@ -1,3 +1,74 @@
 const jwt = require("jsonwebtoken");
 const env = require('../config/env.js');
 const db = require('../config/db.config.js');
+
+let userRole;
+
+verifyToken = (req, res, next) => {
+	let token = req.headers['x-access-token'] || req.headers['authorization'];
+    
+	if(token && token.startsWith('Bearer ')){
+		token = token.slice(7, token.length)
+	}
+	if (!token) {
+	  return res.status(403).send({
+		message: "A token is required for authentication"
+	  });
+	}
+
+	jwt.verify(token, env.JWT_ENCRYPTION, (err, decoded) => {
+	  if(err){
+		console.log(err);
+		return res.status(401).send({
+		  message: "Invalid Token!"
+		});
+	  }else{
+        userRole = decoded.role;
+		console.log("jwt verified")
+		next();
+		//res.status(200).send({ message: "successs" });
+	  }
+	});
+};
+
+isAdmin = (req, res,next) => {
+	if(userRole!=="Admin"){
+		res.status(403).send({
+			message: "Unauthorized Access"
+		});
+		return;
+	}
+	next();
+	return;	
+};
+
+isInstructor = (res) => {
+    if(userRole!=="Instructor"){
+		res.status(403).send({
+			message: "Unauthorized Access"
+		});
+		return;
+	}
+	next();
+	return;
+};
+
+isStudent = (res) => {
+    if(userRole!=="Student"){
+		res.status(403).send({
+			message: "Unauthorized Access"
+		});
+		return;
+	}
+	next();
+	return;
+};
+
+const authJwt = {
+    verifyToken: verifyToken,
+    isAdmin: isAdmin,
+    isInstructor: isInstructor,
+    isStudent: isStudent
+  };
+
+module.exports = authJwt;
